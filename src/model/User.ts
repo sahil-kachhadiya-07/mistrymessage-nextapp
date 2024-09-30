45e28f050d2cdb0f4fa454348daa82d744f8bf83
sahil <sahilkacchadiya@gmail.com>
2024-09-30 15:28:43 +0530

    add email functionality

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,4 +1,4 @@
-import mongoose , {Schema , Document, BooleanExpression} from "mongoose";
+import mongoose , {Schema , Document} from "mongoose";
 
 export interface Message extends Document{
     content:string
